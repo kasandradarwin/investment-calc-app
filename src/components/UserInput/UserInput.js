e41263caa17ed32332adf32943cdedf510b61909
KasandraD <kasandra@jane.app>
2023-06-23 07:10:06 -0700

    added click handlers

--- src/components/UserInput/UserInput.js
+++ src/components/UserInput/UserInput.js
@@ -1,53 +1,71 @@
-import React from 'react'
+import React from "react";
 
 const UserInput = () => {
-    const submitHandler = (event) => {
-        event.preventDefault()
-        
-    }
+  const submitHandler = (event) => {
+    event.preventDefault();
+  };
 
-    const resetHandler = () => {
+  const resetHandler = () => {};
 
-
-    }
-
-    const inputChangeHandler = (input, value) => {
-
-    }
+  const inputChangeHandler = (input, value) => {};
   return (
     <form onSubmit={submitHandler} className="form">
-        <div className="input-group">
-          <p>
-            <label htmlFor="current-savings">Current Savings ($)</label>
-            <input onChange={()=> inputChangeHandler()} type="number" id="current-savings" />
-          </p>
-          <p>
-            <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-            <input onChange={()=> inputChangeHandler()} type="number" id="yearly-contribution" />
-          </p>
-        </div>
-        <div className="input-group">
-          <p>
-            <label htmlFor="expected-return">
-              Expected Interest (%, per year)
-            </label>
-            <input onChange={inputChangeHandler} type="number" id="expected-return" />
-          </p>
-          <p>
-            <label htmlFor="duration">Investment Duration (years)</label>
-            <input onChange={inputChangeHandler} type="number" id="duration" />
-          </p>
-        </div>
-        <p className="actions">
-          <button onClick={resetHandler} type="reset" className="buttonAlt">
-            Reset
-          </button>
-          <button type="submit" className="button">
-            Calculate
-          </button>
+      <div className="input-group">
+        <p>
+          <label htmlFor="current-savings">Current Savings ($)</label>
+          <input
+            onChange={(event) =>
+              inputChangeHandler("current-savings", event.target.value)
+            }
+            type="number"
+            id="current-savings"
+          />
         </p>
-      </form>
-  )
-}
+        <p>
+          <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
+          <input
+            onChange={(event) =>
+              inputChangeHandler("yearly-contribution", event.target.value)
+            }
+            type="number"
+            id="yearly-contribution"
+          />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label htmlFor="expected-return">
+            Expected Interest (%, per year)
+          </label>
+          <input
+            onChange={(event) =>
+              inputChangeHandler("expected-return", event.target.value)
+            }
+            type="number"
+            id="expected-return"
+          />
+        </p>
+        <p>
+          <label htmlFor="duration">Investment Duration (years)</label>
+          <input
+            onChange={(event) =>
+              inputChangeHandler("duration", event.target.value)
+            }
+            type="number"
+            id="duration"
+          />
+        </p>
+      </div>
+      <p className="actions">
+        <button onClick={resetHandler} type="reset" className="buttonAlt">
+          Reset
+        </button>
+        <button type="submit" className="button">
+          Calculate
+        </button>
+      </p>
+    </form>
+  );
+};
 
-export default UserInput
+export default UserInput;
